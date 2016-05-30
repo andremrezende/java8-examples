e22925b175db406a25138cc308fa691425254b0b
Andre Rezende <meirezende@hotmail.com>
2016-05-30 13:00:52 -0300

    Simpified lambdas

--- src/br/com/rezende/java8/example/OrderByString.java
+++ src/br/com/rezende/java8/example/OrderByString.java
@@ -1,6 +1,7 @@
 package br.com.rezende.java8.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,8 +16,12 @@ public class OrderByString {
 		list.add("Andre Rezende");
 		list.add("Darth Vader");
 		list.add("R2D2");
+
+		//list.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 		
-		list.sort((s1, s2)-> Integer.compare(s1.length(), s2.length()));
+		list.sort(Comparator.comparing(s-> s.length()));
 		list.forEach(s -> System.out.println(s));
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
+
 	}
 }
